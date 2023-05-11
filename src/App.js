538f4aa9c1cb8a3f698ab21853994f4200d99d40
HPCarey <hannahpcarey@gmail.com>
2023-05-11 14:38:56 +0000

    test set up and deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>This is a test</p>
+    </div>
+  );
+}
+
+export default App;
