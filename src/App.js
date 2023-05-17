0475677ad6d298d011a8a3b4b2188c39c41b380b
HPCarey <hannahpcarey@gmail.com>
2023-05-17 10:29:13 +0000

    install react bootstrap, add css link to index.html and test output in in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello, World!</h1>
       <p>This is a test</p>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
