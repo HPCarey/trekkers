d686ced71bf1d4bc1be545e96f2a1c16a98ec931
HPCarey <hannahpcarey@gmail.com>
2023-05-21 18:00:38 +0000

    install react-router-dom and create Route in app.js for home, sign in and sign up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import styles from "./App.module.css";
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import {Route,Switch} from 'react-router-dom';
+
 
 function App() {
+
   return (
     <div className={styles.App}>
-      <NavBar>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
 
-      </NavBar>
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,46 @@
 import React from "react";
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
-import logo from '../assets/logo-transparent.png'
-import styles from '../styles/NavBar.module.css'
+import logo from "../assets/logo-transparent.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="logo" height="100" /></Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="logo" height="75" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-            <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
-            <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>
+              Sign In
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
+              <i className="fas fa-user-plus"></i>
+              Sign Up
+            </NavLink>
+
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
