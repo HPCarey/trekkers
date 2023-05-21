b1e77c45492d173c618a0b5db69ada113a3009e5
HPCarey <hannahpcarey@gmail.com>
2023-05-21 12:42:45 +0000

    bugfix: dowgrade react-dom v18 to v17 and replace bootstrap v5 cdn script with v4 in inex.html

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
         <Navbar.Brand href="#home"><img src={logo} alt="logo" height="100" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto text-start">
+          <Nav className="ml-auto text-left">
             <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
             <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
             <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
