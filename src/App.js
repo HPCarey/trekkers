148b555420a3c7276548103463aa9fc665d3cd76
HPCarey <hannahpcarey@gmail.com>
2023-05-23 18:20:53 +0000

    create the PostEdit Form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import PostEditForm from "./pages/posts/PostEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -49,6 +50,8 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+
           <Route exact path="/posts/:id" render={() => <PostPage />} />
 
           <Route render={() => <p>page not found</p>} />
---
+++ src/pages/posts/PostEditForm.js
@@ -0,0 +1,282 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Rating } from "react-simple-star-rating";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+function PostEditForm() {
+  const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    image: "",
+    country: "",
+    location: "",
+    difficulty: "Easy", // Set the default value here
+  });
+
+  const { title, content, image, country, location, difficulty } = postData;
+
+  const [rating, setRating] = useState(0); //set the initial value for the rating
+
+  const imageInput = useRef(null);
+  const history = useHistory();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/${id}/`);
+        const {
+          title,
+          content,
+          image,
+          country,
+          location,
+          difficulty,
+          is_owner,
+        } = data;
+
+        is_owner
+          ? setPostData({
+              title,
+              content,
+              image,
+              country,
+              location,
+              difficulty,
+            })
+          : history.push("/");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [history, id]);
+
+  const handleRating = (rating) => {
+    setRating(rating);
+  };
+  //   const handleRating = (rate) => {
+  //     setRating(rate / 5);
+  //   };
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+//   const handleChangeRating = (event) => {
+//     setRating({
+//       ...postData,
+//       [event.target.name]: event.target.value,
+//     });
+//   };
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("difficulty", difficulty);
+    formData.append("country", country);
+    formData.append("location", location);
+    formData.append("rating", rating);
+
+    if (imageInput?.current?.files[0]) {
+      formData.append("image", imageInput.current.files[0]);
+    }
+    try {
+      await axiosReq.put(`/posts/${id}/`, formData);
+      history.push(`/posts/${id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
+  const textFields = (
+    <div className={`${"text-center"} ${styles.Form}`}>
+      <Form.Group>
+        <Form.Label>Trail name</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Trail difficulty:</Form.Label>
+        <Form.Control
+          name="difficulty"
+          value={difficulty}
+          onChange={handleChange}
+          aria-label="difficulty"
+          as="select"
+          size="sm"
+          custom
+        >
+          <option value="Easy">Easy</option>
+          <option value="Moderate">Moderate</option>
+          <option value="Challenging">Challenging</option>
+          <option value="Difficult">Difficult</option>
+        </Form.Control>
+      </Form.Group>
+      {errors?.difficulty?.map((message, idx) => (
+        <Alert variant="danger" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Country:</Form.Label>
+        <Form.Control
+          type="text"
+          name="country"
+          value={country}
+          onChange={handleChange}
+          aria-label="country"
+        />
+      </Form.Group>
+      {errors?.country?.map((message, idx) => (
+        <Alert variant="danger" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Location:</Form.Label>
+        <Form.Control
+          type="text"
+          name="location"
+          value={location}
+          onChange={handleChange}
+          aria-label="location"
+        />
+      </Form.Group>
+      {errors?.location?.map((message, idx) => (
+        <Alert variant="danger" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Trail description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Rate this trail:</Form.Label>
+        <Rating
+          type="int"
+          name="rating"
+          value={rating}
+          onClick={setRating}
+        />
+        {errors?.rating?.map((message, idx) => (
+          <Alert variant="danger" key={idx}>
+            {message}
+          </Alert>
+        ))}
+      </Form.Group>
+
+      <br />
+
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => history.goBack()}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        save
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              <figure>
+                <Image className={appStyles.Image} src={image} rounded />
+              </figure>
+              <div>
+                <Form.Label
+                  className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                  htmlFor="image-upload"
+                >
+                  Change the image
+                </Form.Label>
+              </div>
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default PostEditForm;
