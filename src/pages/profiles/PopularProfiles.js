9b417ea5b0c724d6bf668a63333437b31722d123
HPCarey <hannahpcarey@gmail.com>
2023-06-20 17:52:13 +0000

    bug: port not working

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { axiosReq } from '../../api/axiosDefaults';
 import Asset from '../../components/Asset';
+import Profile from './Profile';
 
 
 const PopularProfiles = ({mobile}) => {
@@ -42,12 +43,12 @@ const PopularProfiles = ({mobile}) => {
             {mobile ? (
               <div className="d-flex justify-content-around">
                 {popularProfiles.results.slice(0, 4).map((profile) => (
-                  <p key={profile.id}>{profile.owner}</p>
+                  <Profile key={profile.id} profile={profile} mobile />
                 ))}
               </div>
             ) : (
               popularProfiles.results.map((profile) => (
-                <p key={profile.id}>{profile.owner}</p>
+                <Profile key={profile.id} profile={profile} />
               ))
             )}
           </>
