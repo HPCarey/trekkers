8787eb0f926fa2fd33eed8bf34e4151d8d2255a8
HPCarey <hannahpcarey@gmail.com>
2023-06-21 14:45:46 +0000

    add ProfilePage component and wire up in the routes in app.js to display basic profile page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -51,9 +52,8 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
-
           <Route exact path="/posts/:id" render={() => <PostPage />} />
-
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route render={() => <p>page not found</p>} />
         </Switch>
       </Container>
